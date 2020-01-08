60395345c257bf99d68ccd43071795a9ac167523
Florin DUMITRU <dumitruflorin19@yahoo.com>
2020-01-08 11:08:48 +0000

    Add the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -14,23 +14,41 @@ function App() {
       <div className='cover-row-img'>
         <div className='cover-row-item-img'>
           <img src={Avatar} alt='' />
+          <div className='cover-row-item-img-overlay'>
+            <h1>Avatar</h1>
+          </div>
         </div>
         <div className='cover-row-item-img'>
         <img src={CaptainAmerica} alt='' />
+        <div className='cover-row-item-img-overlay'>
+            <h1>Captain America</h1>
+        </div>
         </div>
         <div className='cover-row-item-img'>
         <img src={Endgame} alt='' />
+        <div className='cover-row-item-img-overlay'>
+            <h1>Endgame</h1>
+        </div>
         </div>
       </div>
       <div className='cover-row-img'>
         <div className='cover-row-item-img'>
         <img src={FastandFurios} alt='' />
+        <div className='cover-row-item-img-overlay'>
+            <h1>Fast and Furios</h1>
+        </div>
         </div>
         <div className='cover-row-item-img'>
         <img src={JackRyan} alt='' />
+        <div className='cover-row-item-img-overlay'>
+            <h1>Jack Ryan</h1>
+        </div>
         </div>
         <div className='cover-row-item-img'>
         <img src={Kingsman} alt='' />
+        <div className='cover-row-item-img-overlay'>
+            <h1>Kingsman</h1>
+        </div>
         </div>
       </div>
     </div>
