358366b12db07c8ff73e74a48c63f9ed24ba55a6
Florin DUMITRU <dumitruflorin19@yahoo.com>
2020-01-08 08:56:44 +0000

    Add 6 TV show title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
 import './App.css';
-import MrRobot from './MrRobot.jpg';
+import Avatar from './Avatar.jpeg';
+import CaptainAmerica from './CaptainAmerica.jpeg';
+import Endgame from './Endgame.jpeg';
+import FastandFurios from './FastandFurios.jpeg';
+import JackRyan from './JackRyan.jpeg';
+import Kingsman from './Kingsman.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <img src={MrRobot} className='cover-img' alt='MR Robot show cover' />
+    <div className='App'>
+      <div className='cover-row-img'>
+        <div className='cover-row-item-img'>
+          <h1>Avatar</h1>
+        </div>
+        <div className='cover-row-item-img'>
+          <h1>Captain America</h1>
+        </div>
+        <div className='cover-row-item-img'>
+          <h1>Endgame</h1>
+        </div>
+      </div>
+      <div className='cover-row-img'>
+        <div className='cover-row-item-img'>
+          <h1>Fast and Furios</h1>
+        </div>
+        <div className='cover-row-item-img'>
+          <h1>Jack Ryan</h1>
+        </div>
+        <div className='cover-row-item-img'>
+          <h1>Kingsman</h1>
+        </div>
+      </div>
     </div>
   );
 }
