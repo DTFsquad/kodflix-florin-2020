7fac57a23864c2f76bcded11ddcbbdb6b306f207
Florin DUMITRU <dumitruflorin19@yahoo.com>
2020-01-13 15:18:25 +0000

    Create a Gallery component to show the covers

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Cover from './cover/Cover';
-import Avatar from './cover/images/Avatar.jpeg';
-import CaptainAmerica from './cover/images/CaptainAmerica.jpeg';
-import Endgame from './cover/images/Endgame.jpeg';
-import FastandFurios from './cover/images/FastandFurios.jpeg';
-import JackRyan from './cover/images/JackRyan.jpeg';
-import Kingsman from './cover/images/Kingsman.jpeg';
+import Gallery from './gallery/Gallery';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-     <div className='App'>
-      <div className='cover-row-img'>
-        <Cover 
-          img={Avatar} 
-          title='Avatar' 
-          alt='Cover of Avatar movie' 
-        />
-        <Cover 
-          img={CaptainAmerica} 
-          title='Captain America' 
-          alt='Cover of Captain America movie' 
-        />
-        <Cover 
-          img={Endgame} 
-          title='Endgame' 
-          alt='Cover of Endgame movie' 
-        />
+      <div className='App'>
+        <Gallery />
       </div>
-      <div className='cover-row-img'>
-        <Cover 
-          img={FastandFurios} 
-          title='Fast and Furios' 
-          alt='Cover of Fast and Furios movie' 
-        />
-        <Cover 
-          img={JackRyan} 
-          title='Jack Ryan' 
-          alt='Cover of Jack Ryan movie' 
-        />
-        <Cover 
-          img={Kingsman} 
-          title='Kingsman' 
-          alt='Cover of Kingsman movie' 
-        />
-      </div>
-    </div>
-  );
+    );
+  }
 }
-}
-
-export default App;
---
+++ src/gallery/Gallery.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Gallery.css';
+import Cover from './cover/Cover';
+import Avatar from './cover/images/Avatar.jpeg';
+import CaptainAmerica from './cover/images/CaptainAmerica.jpeg';
+import Endgame from './cover/images/Endgame.jpeg';
+import FastandFurios from './cover/images/FastandFurios.jpeg';
+import JackRyan from './cover/images/JackRyan.jpeg';
+import Kingsman from './cover/images/Kingsman.jpeg';
+
+export default class App extends React.Component {
+    render() {
+      return (
+       <div className='App'>
+        <div className='cover-row-img'>
+          <Cover 
+            img={Avatar} 
+            title='Avatar' 
+            alt='Cover of Avatar movie' 
+          />
+          <Cover 
+            img={CaptainAmerica} 
+            title='Captain America' 
+            alt='Cover of Captain America movie' 
+          />
+          <Cover 
+            img={Endgame} 
+            title='Endgame' 
+            alt='Cover of Endgame movie' 
+          />
+        </div>
+        <div className='cover-row-img'>
+          <Cover 
+            img={FastandFurios} 
+            title='Fast and Furios' 
+            alt='Cover of Fast and Furios movie' 
+          />
+          <Cover 
+            img={JackRyan} 
+            title='Jack Ryan' 
+            alt='Cover of Jack Ryan movie' 
+          />
+          <Cover 
+            img={Kingsman} 
+            title='Kingsman' 
+            alt='Cover of Kingsman movie' 
+          />
+        </div>
+      </div>
+    );
+  }
+  }
